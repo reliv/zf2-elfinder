d230ba0fb17305ccebc8e049b844368787d11f60
Westin Shafer <wshafer@relivinc.com>
2014-10-22 07:48:51 -0500

    Fix for asset manager and EL-Finder

--- config/module.config.php
+++ config/module.config.php
@@ -139,5 +139,12 @@ return array(
         ),
     ),
 
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'aliases' => array(
+                'modules/el-finder/' => __DIR__ . '/../public/',
+            ),
+        ),
+    ),
 
 );
