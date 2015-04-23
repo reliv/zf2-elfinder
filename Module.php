87be55e60f909f9a8c5a6f5f3ac5f9c5240b986b
Westin Shafer <wshafer@relivinc.com>
2015-04-23 08:44:18 -0500

    Prepping for upgrade of Elfinder

--- Module.php
+++ Module.php
@@ -32,27 +32,6 @@ namespace ElFinder;
 class Module
 {
     /**
-     * getAutoloaderConfig() is a requirement for all Modules in ZF2.  This
-     * function is included as part of that standard.  See Docs on ZF2 for more
-     * information.
-     *
-     * @return array Returns array to be used by the ZF2 Module Manager
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__ . '/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    /**
      * getConfig() is a requirement for all Modules in ZF2.  This
      * function is included as part of that standard.  See Docs on ZF2 for more
      * information.
